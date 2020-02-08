5b30c02065d6daf64442cfb79f4f348088ca8caf
julienbassin <julienbassin@outlook.com>
2020-02-08 19:49:31 +0100

    Update ProductController.cs
    update P2-DI

--- P2dotnetApp/P2FixAnAppDotNetCode/Controllers/ProductController.cs
+++ P2dotnetApp/P2FixAnAppDotNetCode/Controllers/ProductController.cs
@@ -9,28 +9,17 @@ namespace P2FixAnAppDotNetCode.Controllers
     {
         private readonly IProductService _productService;
         private readonly ILanguageService _languageService;
-        private readonly IProductServiceTest _productServiceTest;
-
-        public ProductController(IProductService productService, ILanguageService languageService , IProductServiceTest productServiceTest)
+        
+        public ProductController(IProductService productService, ILanguageService languageService)
         {
             _productService = productService;
             _languageService = languageService;
-            _productServiceTest = productServiceTest;
         }
 
         public IActionResult Index()
-        {
-          
-            if (_productServiceTest.RestoreProductValue().Count != 0)
-            {                
-                List<Product> products = _productServiceTest.RestoreProductValue();
-                return View(products);
-            }
-            else
-            {
+        {           
                 IEnumerable<Product> products = _productService.GetAllProducts();
-                return View(products);
-            }           
+                return View(products);        
 
         }
     }
